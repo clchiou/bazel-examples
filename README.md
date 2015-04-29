fe7824c49ccbc0878e4603e6ccc7cdb2087b2808
Che-Liang Chiou <clchiou@gmail.com>
2015-04-28 23:23:38 -0700

    Add x264

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 My collection of [Bazel](http://bazel.io) BUILD examples.
 
-Run `./prepare.sh` to download dependencies.
+Please first run `./install-deps.sh` to install dependencies if your
+are using Debian or Ubuntu, or use the package system you are using.
+
+Then run `./prepare.sh` to download source code tarballs.
--- prepare.sh
+++ prepare.sh
@@ -6,6 +6,9 @@ YASM="yasm-1.3.0"
 YASM_TARBALL="yasm-1.3.0.tar.gz"
 YASM_URL="http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz"
 
+X264_TARBALL="last_x264.tar.bz2"
+X264_URL="http://download.videolan.org/pub/x264/snapshots/last_x264.tar.bz2"
+
 main() {
   if [ ! -d ".git" ]; then
     echo "$0 has to be ran under the root directory"
@@ -15,6 +18,7 @@ main() {
   local ROOT="$(pwd)"
 
   mkdir -p "${ROOT}/tools"
+  mkdir -p "${ROOT}/x264"
   mkdir -p "${ROOT}/yasm"
 
   echo "### bazel"
@@ -23,6 +27,12 @@ main() {
   for dir in cpp defaults genrule; do
     [ -d "${dir}" ] || ln -s "bazel/tools/${dir}"
   done
+
+  echo "### x264"
+  cd "${ROOT}/x264"
+  [ -f "${X264_TARBALL}" ] || wget "${X264_URL}"
+  [ -d x264-snapshot* ] || tar xjvf "${X264_TARBALL}"
+  [ -e x264 ] || ln -s x264-snapshot* x264
 
   echo "### yasm"
   cd "${ROOT}/yasm"
